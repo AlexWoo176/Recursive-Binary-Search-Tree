96ea29f08038028d648a66344d74cf10e611fc2e
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-04-07 16:32:25 +0700

    complete

---
+++ src/codegym/MainBST.java
@@ -0,0 +1,13 @@
+package codegym;
+
+public class MainBST {
+    public static void main(String[] args) {
+        TreeNode root = null;
+        int[] keys = {60, 55, 100, 45, 57, 67, 107, 50, 59, 101, 109};
+        for (int key : keys) {
+            root = BinarySearchTree.insertValue(root, key);
+        }
+        BinarySearchTree.searchNode(root, 59, null);
+        System.out.println();
+    }
+}
